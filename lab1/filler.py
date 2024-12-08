c450e8062a95f8f26b51f6fe889f7b7bc4b9be11
Mazurov Timofey <mazurovti@student.bmstu.ru>
2024-12-08 14:41:36 +0300

    Changed filler to remove custom date generation function and use faker instead

--- lab1/filler.py
+++ lab1/filler.py
@@ -1,24 +1,14 @@
 import csv
 from faker import Faker
 import random
-from datetime import datetime, timedelta
 
 # Инициализация Faker
 fake = Faker('ru_RU')
 
-# Функция для генерации случайной даты рождения
-def random_birth_date(start_month, end_month):
-    start_date = datetime(year=2024, month=start_month, day=1)
-    end_date = datetime(year=2024, month=end_month, day=30)
-    time_between_dates = end_date - start_date
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    return start_date + timedelta(days=random_number_of_days)
-
 n = int(input("Введите кол-во записей: "))
 
 # Генерация данных для таблицы authors.csv
-with open('/tmp/postgres/authors.csv', 'w', newline='', encoding='utf-8') as csvfile:
+with open('./lab1/authors.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['name', 'birth_date', 'rating', 'bio']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -26,13 +16,13 @@ with open('/tmp/postgres/authors.csv', 'w', newline='', encoding='utf-8') as csv
     for _ in range(n):
         writer.writerow({
             'name': fake.name(),
-            'birth_date': random_birth_date(1900, 1980),
+            'birth_date': fake.date_of_birth(minimum_age=18),
             'rating': round(random.uniform(0, 5), 1),
             'bio': fake.text(max_nb_chars=500)
         })
 
 # Генерация данных для таблицы publishers.csv
-with open('/tmp/postgres/publishers.csv', 'w', newline='', encoding='utf-8') as csvfile:
+with open('./lab1/publishers.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['name', 'address', 'phone', 'licence_number']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -46,7 +36,7 @@ with open('/tmp/postgres/publishers.csv', 'w', newline='', encoding='utf-8') as 
         })
 
 # Генерация данных для таблицы translators.csv
-with open('/tmp/postgres/translators.csv', 'w', newline='', encoding='utf-8') as csvfile:
+with open('./lab1/translators.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['name', 'birth_date', 'bio', 'native_language', 'rating']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -54,14 +44,14 @@ with open('/tmp/postgres/translators.csv', 'w', newline='', encoding='utf-8') as
     for _ in range(n):
         writer.writerow({
             'name': fake.name(),
-            'birth_date': random_birth_date(1950, 2023),
+            'birth_date': fake.date_of_birth(minimum_age=18),
             'bio': fake.text(max_nb_chars=300),
             'native_language': random.choice(['English', 'Spanish', 'French', 'German', 'Italian']),
             'rating': round(random.uniform(0, 5), 1)
         })
 
 # Генерация данных для таблицы books.csv
-with open('/tmp/postgres/books.csv', 'w', newline='', encoding='utf-8') as csvfile:
+with open('./lab1/books.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['title', 'publication_date', 'isbn', 'genre', 'rating', 'author_id', 'publisher_id', 'translator_id']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -69,7 +59,7 @@ with open('/tmp/postgres/books.csv', 'w', newline='', encoding='utf-8') as csvfi
     for _ in range(n):
         writer.writerow({
             'title': fake.sentence(nb_words=random.randint(3, 8)),
-            'publication_date': random_birth_date(1980, 2023),
+            'publication_date': fake.date_of_birth(maximum_age=70),
             'isbn': random.randint(1000000000000, 9999999999999),
             'genre': random.choice(['Fiction', 'Non-fiction', 'Biography', 'History', 'Science']),
             'rating': round(random.uniform(0, 5), 1),
